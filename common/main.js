80cca26c6263bf1b4ff5b1a88acb21d049c02b6e
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2022-09-07 17:31:58 +0800

    Initial commit

---
+++ common/main.js
@@ -0,0 +1,3 @@
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+<script type="text/javascript" src="https://fastly.jsdelivr.net/npm/echarts@5.3.3/dist/echarts.min.js"></script>
+<script src="https://cdn.bootcdn.net/ajax/libs/axios/0.27.2/axios.min.js"></script>
